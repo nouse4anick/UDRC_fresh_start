93e416c14c6ad7ceb5f206e3a0083ec98dac86eb
Art Miller - KC7SDA <git@nouse4anick.com>
2017-06-29 20:13:14 -0700

    release - update script
    quick and dirty upgrade script, will remove old version of fldigi and
    download and install the new version after all the other deps get
    downloaded and installed

--- update.sh
+++ update.sh
@@ -11,20 +11,29 @@
 #note: might need to delete current install of fldigi????
 #ok, according to io group for fldigi you have to uninstall the current version, so we need to find a way to do that
 #but for now lets just get it over with
-#cd ~/fldigi-4.0.4
-#sudo make uninstall
-#cd ..
+cd ~/fldigi-4.0.4
+sudo make uninstall
+cd ..
 #download and install the new one
-#wget -N https://sourceforge.net/projects/fldigi/files/fldigi/fldigi-4.0.6.tar.gz
-#tar -zxvsf fldigi-4.0.6.tar.gz
-#cd fldigi-4.0.6
+wget -N https://sourceforge.net/projects/fldigi/files/fldigi/fldigi-4.0.6.tar.gz
+tar -zxvsf fldigi-4.0.6.tar.gz
+cd fldigi-4.0.6
 #BEFORE INSTALL, get all the deps for it!!! this takes editing the source list file and other fun stuff
 sudo cp /etc/apt/sources.list /etc/apt/sources.4.0.6.bkup
-sudo sed '3,3s/.//' /etc/apt/sources.list | sudo tee /etc/apt/sources.list
+#dirty way of doing it
+echo  "deb http://mirrordirector.raspbian.org/raspbian/ jessie main contrib non-free rpi
+# Uncomment line below then 'apt-get update' to enable 'apt-get source'
+deb-src http://archive.raspbian.org/raspbian/ jessie main contrib non-free rpi
+" | sudo tee /etc/apt/sources.list
+echo "sources.list backed up to sources.4.0.6.bkup, please add any other sources from the old file to the new one that are not already in there"
+#tried to use sed and tee to remove the 3rd line but no go =(
+#best method would be to just append the deb-src line to the bottom of the list but that also has issues
+#sudo sed '3,3s/.//' /etc/apt/sources.list | sudo tee /etc/apt/sources.list
+
 #update and build the deps for fldigi
 sudo apt-get update
 sudo apt-get build-dep fldigi -y
 # now we can configure and install
-#./configure
-#make
-#sudo make install
+./configure
+make
+sudo make install
