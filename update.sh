ee376501a7b2178310dfb6f6939161c078b7c288
Art Miller - KC7SDA <git@nouse4anick.com>
2017-07-28 20:37:40 -0700

    clean and enhance
    added functionality for current and previous versions, now checks for a
    previous version if if not found skips over trying to uninstall it.

--- update.sh
+++ update.sh
@@ -2,22 +2,17 @@
 # this script updates a previous install script from a past install
 # you should NOT use this script if you are starting out fresh!
 # 
+#current/past versions:
+#note: this script will SKIP any item that has the same current and previous version numbers!
+FLDIGICUR=4.0.8
+FLDIGIPREV=4.0.6
 
-# command scratch pad:
-# sudo apt-get install libxft-dev # from fldigi wiki
-# sudo apt-get build-dep fldigi
-#sudo apt-get -y install git cmake build-essential libusb-1.0-0.dev libltdl-dev libusb-1.0-0 libhamlib-utils libsamplerate0 libsamplerate0-dev libsigx-2.0-dev libsigc++-1.2-dev libpopt-dev tcl8.5-dev libspeex-dev libasound2-dev alsa-utils libgcrypt11-dev libpopt-dev libfltk1.3-dev libpng++-dev libportaudio-dev libpulse-dev libportaudiocpp0
+FLAMPCUR=2.2.03
+FLAMPPREV=2.2.03
 
-#note: might need to delete current install of fldigi????
-#ok, according to io group for fldigi you have to uninstall the current version, so we need to find a way to do that
-#but for now lets just get it over with
-cd ~/fldigi-4.0.4
-sudo make uninstall
-cd ..
-#download and install the new one
-wget -N https://sourceforge.net/projects/fldigi/files/fldigi/fldigi-4.0.6.tar.gz
-tar -zxvsf fldigi-4.0.6.tar.gz
-cd fldigi-4.0.6
+FLMSGCUR=4.0.3
+FLMSGPREV=4.0.2
+
 #BEFORE INSTALL, get all the deps for it!!! this takes editing the source list file and other fun stuff
 sudo cp /etc/apt/sources.list /etc/apt/sources.4.0.6.bkup
 #dirty way of doing it
@@ -29,33 +24,57 @@ echo "sources.list backed up to sources.4.0.6.bkup, please add any other sources
 #tried to use sed and tee to remove the 3rd line but no go =(
 #best method would be to just append the deb-src line to the bottom of the list but that also has issues
 #sudo sed '3,3s/.//' /etc/apt/sources.list | sudo tee /etc/apt/sources.list
-
 #update and build the deps for fldigi
 sudo apt-get update
 sudo apt-get build-dep fldigi -y
-# now we can configure and install
-./configure
-make
-sudo make install
-cp data/fldigi.desktop ~/Desktop/
-cp data/flarq.desktop ~/Desktop/
 
+cd ~
+if [ "$FLDIGIPREV" != "$FLDIGICUR" ]; then
+	if [ -d "~/fldigi-$FLDIGIPREV" ]; then
+		cd ~/fldigi-$FLDIGIPREV
+		sudo make uninstall
+		cd ..
+	fi
+	#download and install the new one
+	wget -N https://sourceforge.net/projects/fldigi/files/fldigi/fldigi-$FLDIGICUR.tar.gz
+	tar -zxvsf fldigi-$FLDIGICUR.tar.gz
+	cd fldigi-$FLDIGICUR
+
+	# now we can configure and install
+	./configure
+	make
+	sudo make install
+	# cpy the desktop shortcuts
+	cp data/fldigi.desktop ~/Desktop/
+	cp data/flarq.desktop ~/Desktop/
+fi
 #install flamp and flmsg
-
-cd ..
-wget -N https://sourceforge.net/projects/fldigi/files/flamp/flamp-2.2.03.tar.gz
-wget -N https://sourceforge.net/projects/fldigi/files/flmsg/flmsg-4.0.2.tar.gz
-tar -zxvsf flamp-2.2.03.tar.gz
-tar -zxvsf flmsg-4.0.2.tar.gz
-
-cd flamp-2.2.03
-./configure
-make
-sudo make install
-cp data/flamp.desktop ~/Desktop
-
-cd ../flmsg-4.0.2
-./configure
-make
-sudo make install
-cp data/flmsg.desktop ~/Desktop/
+cd ~
+if [ "$FLAMPPREV" != "$FLAMPCUR" ]; then
+	if [ -d "~/flamp-$FLAMPPREV" ]; then
+		cd flamp-$FLAMPPREV
+		sudo make uninstall
+		cd ..
+	fi
+	wget -N https://sourceforge.net/projects/fldigi/files/flamp/flamp-$FLAMPCUR.tar.gz
+	tar -zxvsf flamp-$FLAMPCUR.tar.gz
+	cd flamp-$FLAMPCUR
+	./configure
+	make
+	sudo make install
+	cp data/flamp.desktop ~/Desktop
+fi
+if [ "$FLMSGPREV" != "$FLMSGCUR" ]; then
+	if [ -d "~/flmsg-$FLMSGPREV" ]; then
+		cd flmsg-$FLMSGPREV
+		sudo make uninstall
+		cd ..
+	fi
+	wget -N https://sourceforge.net/projects/fldigi/files/flmsg/flmsg-$FLMSGCUR.tar.gz
+	tar -zxvsf flmsg-$FLMSGCUR.tar.gz
+	cd ../flmsg-$FLMSGCUR
+	./configure
+	make
+	sudo make install
+	cp data/flmsg.desktop ~/Desktop/
+fi
